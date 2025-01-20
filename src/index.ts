461152fea437cc8c43c68f012d04e883b88285f3
nakao keisuke <r3grashopper193@gmail.com>
2025-01-20 23:10:29 +0900

    WIP:user create api作成中

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { eq } from 'drizzle-orm';
 import { usersTable } from './db/schema';
-
-const db = drizzle(process.env.DATABASE_URL!);
+import { Hono } from 'hono';
 
 async function main() {
     const user: typeof usersTable.$inferInsert = {
@@ -39,3 +38,19 @@ async function main() {
 }
 
 main();
+
+type Bindings = {
+    DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+const db = drizzle(process.env.DATABASE_URL!);
+
+app.post("/users", async (c) => {
+    const params = await c.req.json<typeof usersTable.$inferSelect>();
+
+    const result = await db.insert(usersTable).values(params);
+    return c.json(result);
+});
+
+export default app;
